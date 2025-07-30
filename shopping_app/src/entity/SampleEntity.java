8b7b1ded835f989e33857b14b0f5dd4dcd2aa1c6
Bala Krishnan .s <balakrishnan206k@gmail.com>
2025-07-30 13:51:23 +0530

    Update SampleEntity.java

--- shopping_app/src/entity/SampleEntity.java
+++ shopping_app/src/entity/SampleEntity.java
@@ -5,7 +5,7 @@ public class SampleEntity {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome...");
-		System.out.println("Hello World");
+		System.out.println("Hello team");
 	}
 
 }
