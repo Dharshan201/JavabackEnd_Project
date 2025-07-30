7b7b2dcd88e97bbfc7b12bdc3e8ee5031b50379b
MUGENDIRAN <mugendirann@gmail.com>
2025-07-30 13:54:05 +0530

    Update SampleEntity.java

--- shopping_app/src/entity/SampleEntity.java
+++ shopping_app/src/entity/SampleEntity.java
@@ -5,7 +5,8 @@ public class SampleEntity {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome...");
-		System.out.println("Hello World");
+		System.out.println("Hello ");
+		System.out.println("hi");
 	}
 
 }
